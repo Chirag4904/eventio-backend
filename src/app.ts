91475d4b59ce04423013e019a3709c644f261fe8
Chirag4904 <chiragaggarwal69@gmail.com>
2025-10-18 04:23:27 +0530

    refactor(routes): move authenticate middleware to user routes only
    The authenticate middleware was previously applied globally to user routes. Now it's moved to route-level for '/me' endpoint only, allowing other user routes to remain public

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import eventRoutes from "./routes/event.routes.ts";
 import healthRoutes from "./routes/health.routes.ts";
 import authRoutes from "./routes/auth.routes.ts";
-import {authenticate} from "./middlewares/authenticate.ts";
+import { authenticate } from "./middlewares/authenticate.ts";
 import userRoutes from "./routes/user.routes.ts";
 
 dotenv.config();
@@ -14,12 +14,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 // Routes
 app.use("/api/health", healthRoutes);
-app.use("/api/user", userRoutes);
+// mount user routes without global auth, route-level middleware will protect '/me'
+app.use("/api/user", authenticate,userRoutes);
 app.use("/api/events", authenticate, eventRoutes);
-
 
 export default app;
